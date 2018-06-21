98e3d1c1822141c42c0d3a2c4cef4acafa1356fd
Brad Carlson <b2carlso@yahoo.com>
2018-06-21 18:47:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
-$(document).ready(function (){
-});
+function searchRepositories() {
+    $(document).ready(function() {
+        const q = $('#searchTerms').val();
+        console.log(q);
+        $.get(`https://api.github.com/search/repositories?q=${q}`, function(data) {
+                $("#results").html(data.items.map(r => renderRepoData(r)));
+            })
+            .fail(error => { displayError() });
+    });
+}
+
+
+function renderRepoData(repo) {
+    return `
+        <h4>Repository Info</h4>
+        Name:${repo.name} </br>
+        Description:${repo.description}</br>
+        Url: <a href="#"> ${repo.html_url}</a>
+        <h4>Owner Info</h4>
+        Avatar: <img src="${repo.owner.avatar_url}"> </br>
+        Name: ${repo.owner.login} </br>
+        Url: <a href="#"> ${repo.owner.html_url}"</a> </br>
+        <a href="#" data-owner="${repo.owner.login}" data-repository="${repo.name}" onclick="showCommits(this)">Show Commits</a>
+        `
+}
+
+function showCommits(el) {
+    const owner = el.dataset.owner;
+    const repo = el.dataset.repository;
+    $(document).ready(function() {
+        $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function(data) {
+                $("#details").html(data.map(c => renderCommitData(c)));
+            })
+            .fail(error => { displayError() });
+    });
+}
+
+function renderCommitData(commits) {
+    return `
+    <p>SHA: ${commits.sha}</p>
+    <p>Author: ${commits.author.login}</p>
+    <p>Avatar: <img src="${commits.author.avatar_url}"></p>
+    `
+}
+
+function displayError() {
+    $("#errors").html("I'm sorry, there's been an error. Please try again.")
+}
